d93a7de96e8d2ef340d1eec0d01a926ace116fcf
soyeone <35295141+soyeone@users.noreply.github.com>
2018-08-29 18:02:55 +0900

    realbasicUI
    very first upload

---
+++ Assets/Scripts/ChangePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePanel : MonoBehaviour {
+    public GameObject myself;
+    public GameObject c2;
+    public GameObject c3;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changecanvas() {
+
+        myself.SetActive(true);
+        c2.SetActive(false);
+        c3.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+
+
+    public void scenechange() {
+        SceneManager.LoadScene("Main_scene");
+    }
+
+    
+}
