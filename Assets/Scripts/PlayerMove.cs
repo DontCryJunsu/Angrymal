335d80b5e6766bc4deac1b2651624d4843a871e8
LimFull <full7002@naver.com>
2018-09-10 15:57:53 +0900

    이동속도(speed) 변수 추가, 이동 딜레이 없음

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,9 @@ public class PlayerMove : MonoBehaviour
 
     int[,] Animal = new int[7, 10];
     int x, y;
+    float Move_Delay = 0.0f;
+    float speed=10;
+    float Move_Max=0;
     // Use this for initialization
     void Start()
     {
@@ -29,46 +32,81 @@ public class PlayerMove : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.5f); // 이동시 딜레이
+            Move_Max = 0;
+            yield return new WaitForSeconds(Move_Delay); // 이동시 딜레이
             if (MapArray.Map[x + 1, y] == 0) //오른쪽
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.forward);
                 x++;
-                for (int i = 0; i < 20; i++)
-                {
-                    transform.Translate(new Vector3(0.25f, 0, 0), Space.World);
+                
+                while (Move_Max < 5) { 
+                    transform.position += (new Vector3(0.01f*speed, 0, 0));   
+                    Move_Max += (0.01f * speed);
                     yield return new WaitForSeconds(0.01f);
                 }
+                /*  //speed 없는 버전
+               for (int i = 0; i < 20; i++)
+               {
+                   transform.Translate(new Vector3(0.25f, 0, 0), Space.World);
+                   yield return new WaitForSeconds(0.01f);
+               }
+               */
+
+
             }
             else if (MapArray.Map[x - 1, y] == 0) //왼쪽
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.back);
                 x--;
+                while (Move_Max < 5)
+                {
+                    transform.position += (new Vector3(-0.01f * speed, 0, 0));   
+                    Move_Max += (0.01f * speed);
+                    yield return new WaitForSeconds(0.01f);
+                }
+                /*
                 for (int i = 0; i < 20; i++)
                 {
                     transform.Translate(new Vector3(-0.25f, 0, 0), Space.World);
                     yield return new WaitForSeconds(0.01f);
                 }
+                */
             }
             else if (MapArray.Map[x, y + 1] == 0) //위쪽
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.left);
                 y++;
+                while (Move_Max < 5)
+                {
+                    transform.position += (new Vector3(0, 0, 0.01f * speed));
+                    Move_Max += (0.01f * speed);
+                    yield return new WaitForSeconds(0.01f);
+                }
+                /*
                 for (int i = 0; i < 20; i++)
                 {
                     transform.Translate(new Vector3(0, 0, 0.25f), Space.World);
                     yield return new WaitForSeconds(0.01f);
                 }
+                */
             }
             else if (MapArray.Map[x, y - 1] == 0) //아래쪽
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.right);
                 y--;
+                while (Move_Max < 5)
+                {
+                    transform.position += (new Vector3(0, 0, -0.01f * speed));
+                    Move_Max += (0.01f * speed);
+                    yield return new WaitForSeconds(0.01f);
+                }
+                /*
                 for (int i = 0; i < 20; i++)
                 {
                     transform.Translate(new Vector3(0, 0, -0.25f), Space.World);
                     yield return new WaitForSeconds(0.01f);
                 }
+                */
             }
         }
 
