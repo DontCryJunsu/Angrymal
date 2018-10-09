c911812309c4ec80ea345d8d889299164e19d645
LimFull <full7002@naver.com>
2018-10-09 18:56:51 +0900

    바닥 작업 시작

---
+++ Assets/Scripts/BlockColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockColor : MonoBehaviour {
+    public GameObject block;
+
+   
+	// Use this for initialization
+	void Start () {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        
+        
+        renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        if (other.gameObject.tag == "blueteam")
+        {
+            renderer.material.color = Color.blue;
+            renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 255);
+        }
+    }
+}
