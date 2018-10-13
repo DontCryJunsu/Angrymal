f851714b66773e4389176e8b397c2c5994a2b95c
soyeone <35295141+soyeone@users.noreply.github.com>
2018-10-13 23:19:06 +0900

    paintingtest
    ..

---
+++ Assets/Scripts/testpainting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testpainting : MonoBehaviour {
+
+    public Transform baseDot;
+    public GameObject ob;
+
+    public int i = 2;
+    public float destsec = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        StartCoroutine("Makedot");
+
+	}
+
+    IEnumerator Makedot() 
+    {
+        Vector3 objPosition = ob.transform.position;
+        Instantiate(baseDot, objPosition, baseDot.rotation);
+        yield return new WaitForSeconds(2f);
+    }
+
+
+}
