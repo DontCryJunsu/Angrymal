e7d12b8748e80a62e288bba96b7f2a19bf91e807
soyeone <35295141+soyeone@users.noreply.github.com>
2019-01-03 23:12:30 +0900

    cat수정본
    수정본

---
+++ Assets/Scripts/testforwalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testforwalk : MonoBehaviour {
+    public enum a_state {
+        walk=0
+
+    }
+    public Animator ani;
+    public a_state state = a_state.walk;
+    public float speed = 1.0f;
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+        if (ani != null) {
+            ani.SetInteger("a_state", (int)state);
+        }
+    }
+}
