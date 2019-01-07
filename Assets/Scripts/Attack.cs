3688ee4f804cf9223ed1e19ead1bfb32f1a25144
LimFull <full7002@naver.com>
2019-01-07 15:24:57 +0900

    공격 만드는 중

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum TARGET
+{
+    MASTER = 0,
+    CLIENT = 1,
+}
+
+public class Attack : MonoBehaviour {
+
+ 
+
+        void OnTriggerEnter(Collider other)
+        {
+        Debug.Log("충돌");
+            if (tag == "redattack")
+            {
+                if (other.tag == "bluecharacter")
+                {
+                    NetAttackDamage(other, gameObject.transform.parent.GetComponent<DefaultMove>().power);
+                }
+            }
+            else if (tag == "blueattack")
+            {
+                if (other.tag == "redcharacter")
+                {
+                    NetAttackDamage(other, gameObject.transform.parent.GetComponent<DefaultMove>().power);
+                }
+            }
+             
+        }
+
+
+
+
+        public void NetAttackDamage(Collider col, float damage)
+        {
+            // 공격시의 데미지를 상대에게 전달함.
+            int attackTarget = 0;
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                attackTarget = (int)TARGET.CLIENT;
+                // 공격하는 대상이 방원일 때
+            }
+            else
+            {
+                attackTarget = (int)TARGET.MASTER;
+                // 공격하는 대상이 방장일 때
+            }
+
+            col.gameObject.GetComponent<PhotonView>().RPC("AttackInfo", PhotonTargets.Others, attackTarget, damage);
+
+            // AttackInfo 는  함수이름입니다.
+            //PhotonTargets.Others 자신을 제외한 다른 유저에게 정보를 전송합니다.damage 는 전달하려고 하는 값입니다.
+        }
+
+
+
+    
+}
