f13e3cd6aba450f481bf467d240eb76ceb417d5d
LimFull <full7002@naver.com>
2019-01-20 19:56:10 +0900

    스토리

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        Debug.Log("씬 체인지");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StoryUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMove = 50 * Time.deltaTime; //y축으로 이동할양 
+        this.transform.Translate(new Vector3(0, yMove, 0));  //이동
+    }
+}
