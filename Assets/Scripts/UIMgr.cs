349882e5667714360a102d551ac0c752dca7ee5b
Dontcryjunsu <bassoff@naver.com>
2019-01-21 19:16:05 +0900

    로비변경중

---
+++ Assets/Scripts/UIMgr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour
+{
+    public Text Name;
+    public Text Bana;
+    void Start()
+    {
+        Name.text = PlayerPrefs.GetString("userName","사용자") + " <color=black>부족</color>";
+        Bana.text = PlayerPrefs.GetInt("Money",0).ToString();
+    }
+}
